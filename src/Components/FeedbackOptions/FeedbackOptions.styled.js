3c4e62f1812198b60d67a70f9baf827ee7fca804
ScreameRRR <68212647+ScreameRRR@users.noreply.github.com>
2022-04-04 00:51:57 +0300

    hw4 feedback is done

---
+++ src/Components/FeedbackOptions/FeedbackOptions.styled.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+const FeedbackOptionsStyled = styled.div`
+  ul {
+    list-style: none;
+    display: flex;
+    margin: 0;
+    padding: 0;
+  }
+
+  button {
+    cursor: pointer;
+    background: none;
+    border-radius: 5px;
+  }
+
+  button:hover {
+    background-color: blue;
+    color: white;
+  }
+
+  li:not(:last-child) {
+    margin-right: 20px;
+  }
+`;
+
+export default FeedbackOptionsStyled;
